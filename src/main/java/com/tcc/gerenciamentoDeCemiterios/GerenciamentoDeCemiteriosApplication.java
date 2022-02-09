cc7b3c746b3a46efe4270ce658473cd6ee41a8eb
Carlos Eduardo <carloseduardopintofilho@hotmail.com>
2022-02-09 15:34:33 -0300

    Projeto criado

---
+++ src/main/java/com/tcc/gerenciamentoDeCemiterios/GerenciamentoDeCemiteriosApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.gerenciamentoDeCemiterios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeCemiteriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeCemiteriosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/gerenciamentoDeCemiterios/GerenciamentoDeCemiteriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.gerenciamentoDeCemiterios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeCemiteriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
